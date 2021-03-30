90aa1102f2d23443f76a57d5804f9018ce20df29
Ryan Kane <ryankane@cmail.carleton.ca>
2021-03-30 14:37:49 -0400

    updating to include sectioning multi-agent algorithm

--- Graph.py
+++ Graph.py
@@ -1,13 +1,18 @@
-import random
+import numpy
+import copy
 
-def gen_random_aisle_graph(aisles=1, rows=5, cols=5, rewards=None):
+DEFAULT = 0
+CUMULATIVE = 1
+
+def gen_random_aisle_graph(aisles=1, rows=5, cols=5, theta=2):
+    # use Zipf distribution 
     new_graph = AisleGraph()
     new_graph.aisles = 2
     aisle = []
     for i in range(rows):
         row = []
         for j in range(cols):
-            reward = random.choice(range(0, 10))
+            reward = numpy.random.zipf(theta)
             row.append(Vertex(reward, i+1, j+1))
         aisle.append(row)
     new_graph.aisle = aisle
@@ -20,7 +25,23 @@ class AisleGraph():
         self.aisles = 1
 
     def get(self, i, j):
-        return self.aisle[i][j]
+        return self.aisle[j][i]
+
+    def get_xy(self, x, y):
+        return self.aisle[x-1][y-1]
+    
+    def rows(self):
+        return len(self.aisle)
+
+    def cols(self):
+        return len(self.aisle[0])
+
+    def reset(self):
+        self.set_compare_mode(DEFAULT)
+
+    def set(self, row, col, val):
+        # sets the reward values of the node at location [row, col] to val.
+        self.aisle[row][col].val = val
 
     def __str__(self):
         # display graph as text
@@ -40,41 +61,81 @@ class AisleGraph():
                     lines += "  \n"
         return lines
     
+    def copy(self):
+        return copy.deepcopy(self)
+
     def get_node_list(self):
         node_list = []
         # return a list of all nodes, order does not matter
         for row in self.aisle:
             node_list += row
         return node_list
+    
+    def calc_cumulative_val(self):
+        for row in self.aisle:
+            c = 0
+            for v in row:
+                c += v.val
+                v.c_val = c
+    
+    def set_compare_mode(self, mode):
+        if mode == CUMULATIVE:
+            self.calc_cumulative_val()
+        for row in self.aisle:
+            for v in row:
+                v.compare = mode
 
     
 class Vertex():
     # a vertex on the graph containing a single value
-    def __init__(self, val, pos_x, pos_y):
+    def __init__(self, val, pos_x, pos_y, c_val=None):
         super().__init__()
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.val = val
+        self.c_val = c_val
+        self.compare = DEFAULT
 
     def __eq__(self, other):
-        return (self.val == other.val) and \
-            (self.pos_x == other.pos_x) and \
-            (self.pos_y == other.pos_y)
+        if self.compare == CUMULATIVE:
+            return (self.c_val == other.c_val) and \
+                (self.pos_x == other.pos_x) and \
+                (self.pos_y == other.pos_y)
+        else:
+            return (self.val == other.val) and \
+                (self.pos_x == other.pos_x) and \
+                (self.pos_y == other.pos_y)
     
     def __ge__(self, other):
-        return self.val >= other
+        if self.compare == CUMULATIVE:
+            return self.c_val >= other
+        else:
+            return self.val >= other
+        
 
     def __le__(self, other):
-        return self.val <= other
+        if self.compare == CUMULATIVE:
+            return self.c_val <= other
+        else:
+            return self.val <= other
 
     def __gt__(self, other):
-        return self.val > other
+        if self.compare == CUMULATIVE:
+            return self.c_val > other
+        else:
+            return self.val > other
     
     def __lt__(self, other):
-        return self.val < other
+        if self.compare == CUMULATIVE:
+            return self.c_val < other
+        else:
+            return self.val < other
 
     def __str__(self):
         return "val: {}, pos_x: {}, pos_y: {}".format(self.val, self.pos_x, self.pos_y)
     
     def __repr__(self):
         return "(val: {}, pos_x: {}, pos_y: {})".format(self.val, self.pos_x, self.pos_y)
+
+    def __hash__(self):
+        return hash(str(self))
--- agents.py
+++ agents.py
@@ -1,4 +1,5 @@
 from Graph import AisleGraph, Vertex
+import Graph
 
 
 class MaxHeap:
@@ -81,24 +82,223 @@ class Traverser():
     # uses the strategy pattern to select how it will traverse a graph
     #   ie: which algorithm to use
     def __init__(self, graph: AisleGraph, budget: int):
-        super.__init__(self)
+        # super.__init__(self)
+        #Graph
         self.graph = graph
-        self.budget = budget
 
-    def GdyME(self):
+        # Budget 
+        self.initial_budget = budget
+        self.budget = budget
+    
+    def reset(self):
+        # reset Traverser
+        self.budget = self.initial_budget
+        # reset graph
+        self.graph.reset()
+
+    def cost(self, node_set):
+        # The structure of an aisle graph means the shortest path to each nodes is uniqie
+        nodes = node_set
+        costs = {}
+        for node in nodes:
+            if node.pos_y in costs.keys():
+                if node.pos_x > costs[node.pos_y]:
+                    costs[node.pos_y] = node.pos_x - 1
+            else:
+                costs[node.pos_y] = node.pos_x - 1
+        # return sum of costs
+        cost = 0
+        y_max = 0
+        for y in costs.keys():
+            cost += costs[y] - 1
+            if y > y_max:
+                y_max = y
+        cost += y_max
+        # cost to get back to start
+        return cost + y_max + costs[y_max]
+
+
+    def calc_path(self, target_node, current_node, path):
+        # calculate the shortest path from current_node to the node passed
+        #   in the argument
+        # return all nodes on the path to node including node, but not the 
+        #   starting point
+        
+        # firstly, if the node is in the same row, we move towards it
+        next_node = None
+        if current_node.pos_y == target_node.pos_y:
+            j = 1 if current_node.pos_x < target_node.pos_x else -1
+            next_node = self.graph.get_xy(current_node.pos_x + j, current_node.pos_y)
+        # if we are at the aisle
+        elif current_node.pos_x == 1:
+            i = 1 if current_node.pos_y < target_node.pos_y else -1
+            next_node = self.graph.get_xy(current_node.pos_x, current_node.pos_y + i)
+        # if we are not at the aisle and in the wrong row
+        else:
+            next_node = self.graph.get_xy(current_node.pos_x-1, current_node.pos_y)
+        path.append(next_node)
+        return path if next_node == target_node else self.calc_path(target_node, next_node, path)
+
+    # algorithms from paper 1
+
+    def Gdy(self):
         g = self.graph
         # initialize the solution with starting node v[1,1] 
         starting_node = g.get(1, 1)
-        s = [starting_node]
+        s = set()
+        s.add(starting_node)
+        self.current_node = starting_node
         # construct a max heap of all the other vertices according to their rewards
         node_list = g.get_node_list()
-        node_list = node_list.remove(starting_node)
+        node_list.remove(starting_node)
         heap = buildMaxHeap(node_list)
         # iterate through the heap, stop if the cost of the trip exceeds the budget
-        while not heap.empty() and g.cost(s) <= self.budget:
-            pass
-        #   with the largest reward, if it is reachable within budget, travel to it
-        #     when travelling to the vertex, add all intermediate vertices to the solution
-        #     as well since the rewards are collected on the way
-        #   otherwise discard and continue
+        while not heap.empty() and self.cost(s) <= self.budget:
+            # with the largest reward, if it is reachable within budget, travel to it
+            new_node = heap.extractMax()
+            path = self.calc_path(new_node, starting_node, [])
+            s_new = s.copy()
+            s_new.add(new_node)
+            if self.cost(s_new) < self.budget:
+                # when travelling to the vertex, add all intermediate vertices to the solution
+                # as well since the rewards are collected on the way
+                for n in path:
+                    s.add(n) # since s is a set we cant add twice
+            # otherwise discard and continue
+                
         # return the reward for all vertices in the solution space
+        return s, sum_node_set(s)
+
+    def GdyME(self):
+        return self.Gdy()
+
+    def GdyMC(self):
+        self.graph.set_compare_mode(Graph.CUMULATIVE)
+        return self.Gdy()
+
+    def ApxMRE(self):
+        r_max = -1
+        s_1 = set()
+        g = self.graph
+        starting_node = g.get_xy(1,1)
+        s_2 = set()
+        s_2.add(starting_node)
+        # construct a 2d matrix H[i,d]
+        H = []
+        for i in range(g.rows()):
+            # print(i, H)
+            H.append([])
+            for d in range(i+1):
+                H[i].append([])
+                for j in range(g.cols()):
+                    v = g.get(i, j)
+                    H[i][d].append(((v.val/(2*(j)+2*d) if j > 0 else 0), v))
+                    if (2*(j) + 2*d) <= self.budget:
+                        if v.val > r_max:
+                            r_max = v.val
+                            s_1.add(v)
+        i_max = 1
+        while self.cost(s_2) < self.budget:
+            M = []
+            for i in range(1, g.rows() + 1):
+                M.append(i)
+            for i in range(g.rows()):
+                if i <= i_max:
+                    M[i] = pair_max(H[i][0])
+                else:
+                    M[i] = pair_max(H[i][i-i_max])
+
+            v = pair_max(M)
+            s_2new = s_2.copy()
+            s_2new.add(v[1])
+            if self.cost(s_2new) <= self.budget:
+                path = self.calc_path(v[1], starting_node, [])
+                # when travelling to the vertex, add all intermediate vertices to the solution
+                # as well since the rewards are collected on the way
+                for n in path:
+                    for d in range(n.pos_y):
+                        H[n.pos_y-1][d][n.pos_x-1] = (0, n)
+                    s_2.add(n) # since s is a set it will not repeat nodes
+                for k in range(v[1].pos_x, g.cols()):
+                    H[v[1].pos_y-1][0][k] = (v[1].val/(2*(k - (v[1].pos_x-1))), v[1])
+            else:
+                break
+            print(s_2)
+            print(self.cost(s_2))
+            print(M)
+            i_max = max([v[1].pos_y-1, i_max])
+        print(self.cost(s_1), sum_node_set(s_1), s_1)
+        print(self.cost(s_2), sum_node_set(s_2), s_2)
+        return (sum_node_set(s_1), s_1) if sum_node_set(s_1) > sum_node_set(s_2) else (sum_node_set(s_2), s_2)
+        
+    # algorithms from paper 2
+    
+    # Multiagent strategies
+    def sectioning(self, num_agents):
+        # section each part of the vineyard so that the agents
+        # only operate on disjoint subsets of the graph.
+        # For our Aisle Graph we separate by full rows
+        total_reward = sum_node_set(set(self.graph.get_node_list()))
+        self.graph.set_compare_mode(Graph.CUMULATIVE)
+        agents = []
+        j = 1
+        for i in range(num_agents):
+            # track where we started from
+            i = j
+            # calculate section of graph using percentage of reward
+            #  vs percentage of budget each agent will have.
+            s = 0
+            b_agent = int(self.budget / num_agents)
+            p_agent = b_agent / self.budget
+            while s/total_reward < p_agent:
+                # agent takes next row
+                j += 1
+                s += sum_node_set(self.graph.get_xy(-1, j))
+            # get copy of graph
+            temp_graph = self.graph.copy()
+            # set nodes not in the span of rows i,j to 0
+            for row in range(i, j):
+                for col in range(1, temp_graph.cols + 1):
+                    temp_graph.set(row,col,0)
+            new_agent = Traverser(temp_graph, b_agent)
+            agents.append(new_agent)
+        
+        sols = []
+        sol_sets = []
+
+        for agent in agents:
+            sol, sol_set = agent.Gdy()
+            sols.append(sol)
+            sol_sets.append(sol_set)
+        return sum(sols), sol_sets
+
+    def series(self, num_agents):
+        # run single agent algorithm in series, except with a time conflict map
+        # to avoid collision
+        self.graph.set_compare_mode(Graph.CUMULATIVE)
+        
+
+    def parallel(self, num_agents):
+        # this requires a new form of the greedy algorithm because
+        # we need to compute the routes in parallel
+        self.graph.set_compare_mode(Graph.CUMULATIVE)
+
+        # while all the agent tours are not completed
+        # 
+        pass
+
+def sum_node_set(node_set):
+    r = 0
+    for node in node_set:
+        r += node.val
+    return r
+
+def pair_max(l):
+        # l is a list of lists, we want to return the item with the max in the first position
+        cur_max = l[0][0]
+        max_item = l[0]
+        for i in l:
+            if i[0] > cur_max:
+                cur_max = i[0]
+                max_item = i
+        return max_item
